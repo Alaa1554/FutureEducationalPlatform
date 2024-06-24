e654181d2fe7a801c05994db267b14debe25435a
mohamed552002 <mozamezo638@gmail.com>
2024-06-25 00:12:42 +0300

    AddHomeworkEntity

---
+++ FutureEducationalPlatform.Persistence/EntityConfiguration/HomeworkConfiguration/HomeworkConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using FutureEducationalPlatform.Domain.Entities.ExamEntites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureEducationalPlatform.Persistence.EntityConfiguration.HomeworkConfiguration
+{
+    public class HomeworkConfiguration : BaseConfiguration<Homework>
+    {
+        public override void Configure(EntityTypeBuilder<Homework> builder)
+        {
+            base.Configure(builder);
+            builder.Property(h => h.IsActive).HasDefaultValue(true);
+            builder.Property(h => h.Name).HasMaxLength(128);
+        }
+    }
+}
